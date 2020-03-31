a1f4e6a0d9a747d2b79c5e2a225291ba90b1ba38
waterlinqq <a10311036@gmail.com>
2020-03-31 15:31:48 +0800

    61

---
+++ 61. Rotate List.js
@@ -0,0 +1,49 @@
+// 61. Rotate List
+// Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+// Example 1:
+
+// Input: 1->2->3->4->5->NULL, k = 2
+// Output: 4->5->1->2->3->NULL
+// Explanation:
+// rotate 1 steps to the right: 5->1->2->3->4->NULL
+// rotate 2 steps to the right: 4->5->1->2->3->NULL
+// Example 2:
+
+// Input: 0->1->2->NULL, k = 4
+// Output: 2->0->1->NULL
+// Explanation:
+// rotate 1 steps to the right: 2->0->1->NULL
+// rotate 2 steps to the right: 1->2->0->NULL
+// rotate 3 steps to the right: 0->1->2->NULL
+// rotate 4 steps to the right: 2->0->1->NULL
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+  if (head == null) return head;
+  let fast = head;
+  while (k--) {
+    fast = fast.next;
+    if (!fast) fast = head;
+  }
+  let slow = head;
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+
+  fast.next = head;
+  head = slow.next;
+  slow.next = null;
+  return head;
+};
